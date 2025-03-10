27cba9590faa994fc6927349f0f7a3990bdae4ba
Egor <egor.nikonenko29@gmail.com>
2025-03-10 15:54:05 +0300

    Code Refactor

--- transport/src/main/java/com/omnia/transport/CustomEndpoint.java
+++ transport/src/main/java/com/omnia/transport/CustomEndpoint.java
@@ -13,8 +13,10 @@ import java.util.stream.Collectors;
 public class CustomEndpoint<RequestT, ResponseT, ErrorT> implements Endpoint<RequestT, ResponseT, ErrorT> {
     private final Endpoint<RequestT, ResponseT, ErrorT> endpoint;
     private final OmniaSDK sdk;
+
     public CustomEndpoint(Endpoint<RequestT, ResponseT, ErrorT> endpoint, OmniaSDK sdk) {
-        this.endpoint = endpoint; this.sdk = sdk;
+        this.endpoint = endpoint;
+        this.sdk = sdk;
     }
 
     @Override
