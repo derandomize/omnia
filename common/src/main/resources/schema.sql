8c85e601737e53e2d27f401f5c7f4c494e755ed3
Aleksei Tokarev <nonezonyx@gmail.com>
2025-03-27 18:16:43 +0300

    fix: jooq version downgrade + config reimagination

--- common/src/main/resources/schema.sql
+++ common/src/main/resources/schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE index_to_commune
 (
-    index   VARCHAR PRIMARY KEY,
-    commune VARCHAR NOT NULL
+    "index"   VARCHAR PRIMARY KEY,
+    "commune" VARCHAR NOT NULL
 );
 
 
 CREATE TABLE omnia_config
 (
-    key   VARCHAR PRIMARY KEY,
-    value VARCHAR
+    "key"   VARCHAR PRIMARY KEY,
+    "value" VARCHAR
 );
