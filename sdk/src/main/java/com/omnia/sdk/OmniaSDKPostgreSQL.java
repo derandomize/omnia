14893a6ff554cffeb40600aa70fe943e57c5fab4
Aleksei Tokarev <nonezonyx@gmail.com>
2025-03-27 18:48:38 +0300

    feat: jook postgresql implementation for sdk basic methods

---
+++ sdk/src/main/java/com/omnia/sdk/OmniaSDKPostgreSQL.java
@@ -0,0 +1,56 @@
+package com.omnia.sdk;
+
+import com.omnia.common.config.db.Database;
+import com.omnia.common.config.db.PostgresqlParams;
+import com.omnia.jooq.tables.IndexToCommune;
+import com.omnia.common.config.AppConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+
+public class OmniaSDKPostgreSQL implements OmniaSDK {
+    private final DSLContext dsl;
+    private final String featureName;
+
+    public OmniaSDKPostgreSQL(AppConfig  config) {
+        featureName = config.getConfig().getFeatureName();
+        Database dbConfig = config.getDatabase();
+        if (!"postgresql".equalsIgnoreCase(dbConfig.getType())) {
+            throw new IllegalArgumentException("Unsupported database type: " + dbConfig.getType());
+        }
+
+        dbConfig.resolveParams();
+        PostgresqlParams params = (PostgresqlParams) dbConfig.getResolvedParams();
+
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(buildJdbcUrl(params));
+        dataSource.setUsername(params.getUsername());
+        dataSource.setPassword(params.getPassword());
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setMinimumIdle(2);
+
+        this.dsl = DSL.using(dataSource, SQLDialect.POSTGRES);
+    }
+
+    private String buildJdbcUrl(PostgresqlParams params) {
+        return String.format("jdbc:postgresql://%s:%d/%s",
+                params.getHost(),
+                params.getPort(),
+                params.getDatabaseName()
+        );
+    }
+
+    @Override
+    public String transformIndexId(String indexId) {
+        return dsl.select(IndexToCommune.INDEX_TO_COMMUNE.COMMUNE)
+                .from(IndexToCommune.INDEX_TO_COMMUNE)
+                .where(IndexToCommune.INDEX_TO_COMMUNE.INDEX.eq(indexId))
+                .fetchOneInto(String.class);
+    }
+
+    @Override
+    public String getFilterField() {
+        return featureName;
+    }
+}
