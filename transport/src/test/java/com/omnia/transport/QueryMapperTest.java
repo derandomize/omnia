07c65a8077dd8e5132a134fbf12056c321b32a3e
Egor <egor.nikonenko29@gmail.com>
2025-05-10 16:22:55 +0300

    more test

--- transport/src/test/java/com/omnia/transport/QueryMapperTest.java
+++ transport/src/test/java/com/omnia/transport/QueryMapperTest.java
@@ -1,6 +1,7 @@
 package com.omnia.transport;
 import org.junit.jupiter.api.Test;
 import org.opensearch.client.opensearch._types.FieldValue;
+import org.opensearch.client.opensearch._types.query_dsl.MatchQuery;
 import org.opensearch.client.opensearch._types.query_dsl.Query;
 import org.opensearch.client.opensearch._types.query_dsl.BoolQuery;
 import org.opensearch.client.opensearch._types.query_dsl.TermQuery;
@@ -59,5 +60,33 @@ public class QueryMapperTest<RequestT, ResponseT, ErrorT>  {
         assertEquals("debug", result.get("level"));
         assertEquals("log", result.get("type"));
     }
+    @Test
+    void queryToMap_MatchQueryTest() {
+        MatchQuery matchQuery = new MatchQuery.Builder()
+                .field("title")
+                .query(q -> q.stringValue("OpenSearch"))
+                .build();
+        Query query = new Query.Builder()
+                .match(matchQuery)
+                .build();
+        Map<String, String> resultMap = new HashMap<>();
+        mapper.queryToMap(query, resultMap);
+        assertEquals(1, resultMap.size());
+        assertEquals("OpenSearch", resultMap.get("title"));
+    }
 
+    @Test
+    void queryToMap_termQuerytest() {
+        Query query = new Query.Builder()
+                .term(t -> t
+                        .field("title")
+                        .value(v -> v.stringValue("OpenSearch"))
+                )
+                .build();
+
+        Map<String, String> resultMap = new HashMap<>();
+        mapper.queryToMap(query, resultMap);
+        assertEquals(1, resultMap.size());
+        assertEquals("OpenSearch", resultMap.get("title"));
+    }
 }
