5275ebb0af8e611437f14932a0cc10346fdfd046
Egor <egor.nikonenko29@gmail.com>
2025-05-13 16:35:08 +0300

    До сих пор тесты от SDK, но устранена проблема с queryParameters для SearchRequest(остальным кажется и не нужно)

--- transport/src/main/java/com/omnia/transport/OmniaTransport.java
+++ transport/src/main/java/com/omnia/transport/OmniaTransport.java
@@ -1,6 +1,8 @@
 package com.omnia.transport;
 
 import org.opensearch.client.json.JsonpMapper;
+import org.opensearch.client.opensearch._types.query_dsl.Query;
+import org.opensearch.client.opensearch.core.SearchRequest;
 import org.opensearch.client.transport.Endpoint;
 import org.opensearch.client.transport.OpenSearchTransport;
 import org.opensearch.client.transport.Transport;
@@ -9,7 +11,9 @@ import org.opensearch.client.transport.TransportOptions;
 import com.omnia.sdk.OmniaSDK;
 
 import javax.annotation.Nullable;
+import javax.crypto.SealedObject;
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class OmniaTransport implements OpenSearchTransport {
@@ -24,7 +28,15 @@ public class OmniaTransport implements OpenSearchTransport {
     @Override
     public <RequestT, ResponseT, ErrorT> ResponseT performRequest(RequestT request, Endpoint<RequestT, ResponseT, ErrorT> endpoint, @Nullable TransportOptions options) throws IOException {
         final OmniaEndpoint<RequestT, ResponseT, ErrorT> customEndpoint = new OmniaEndpoint<>(endpoint, sdk);
-        return delegate.performRequest(request, customEndpoint, options);
+        if(request instanceof SearchRequest){
+            Query combinedQuery = ((SearchRequest) request).query();
+            List<String> Indexes = customEndpoint.getIndex(endpoint.requestUrl(request));
+            for(var x: Indexes) {
+                combinedQuery = sdk.addIndexFilter(combinedQuery,x );
+            }
+            return delegate.performRequest(((RequestT)new SearchRequest.Builder().query(combinedQuery).index(((SearchRequest) request).index()).build()), customEndpoint, options);
+        }
+        return delegate.performRequest(request,customEndpoint,options);
     }
 
     @Override
