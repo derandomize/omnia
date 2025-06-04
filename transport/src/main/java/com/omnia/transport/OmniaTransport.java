ad81e598247cc71781346cf129e9d4efd2821bfb
Egor <egor.nikonenko29@gmail.com>
2025-06-04 05:12:41 +0300

    add reflection for all RequestT

--- transport/src/main/java/com/omnia/transport/OmniaTransport.java
+++ transport/src/main/java/com/omnia/transport/OmniaTransport.java
@@ -3,6 +3,7 @@ package com.omnia.transport;
 import org.opensearch.client.json.JsonpMapper;
 import org.opensearch.client.opensearch._types.query_dsl.Query;
 import org.opensearch.client.opensearch.core.SearchRequest;
+import org.opensearch.client.opensearch.indices.CreateIndexRequest;
 import org.opensearch.client.transport.Endpoint;
 import org.opensearch.client.transport.OpenSearchTransport;
 import org.opensearch.client.transport.Transport;
@@ -29,40 +30,40 @@ public class OmniaTransport implements OpenSearchTransport {
     public <RequestT, ResponseT, ErrorT> ResponseT performRequest(RequestT request, Endpoint<RequestT, ResponseT, ErrorT> endpoint, @Nullable TransportOptions options) throws IOException {
         final OmniaEndpoint<RequestT, ResponseT, ErrorT> customEndpoint = new OmniaEndpoint<>(endpoint, sdk);
         QueryMapper mapper = new QueryMapper();
-        if (request instanceof SearchRequest) {
-            Query combinedQuery = ((SearchRequest) request).query();
-            List<String> Indexes = customEndpoint.getIndex(endpoint.requestUrl(request));
-            for (var x : Indexes) {
-                combinedQuery = sdk.addIndexFilter(combinedQuery, x);
-            }
-            try {
-                mapper.updatePrivateFields(request, combinedQuery);
-                return delegate.performRequest(request, customEndpoint, options);
-            } catch (IllegalAccessException |NoSuchFieldException e) {
-                throw new RuntimeException(e);
-            }
+        Query combinedQuery = (Query) mapper.executeQuery(request);
+        if(combinedQuery == null){
+            return delegate.performRequest(request, customEndpoint, options);
         }
-        return delegate.performRequest(request, customEndpoint, options);
+        List<String> Indexes = customEndpoint.getIndex(endpoint.requestUrl(request));
+        for (var x : Indexes) {
+            combinedQuery = sdk.addIndexFilter(combinedQuery, x);
+        }
+        try {
+            mapper.updatePrivateFields(request, combinedQuery);
+            return delegate.performRequest(request, customEndpoint, options);
+        } catch (IllegalAccessException |NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public <RequestT, ResponseT, ErrorT> CompletableFuture<ResponseT> performRequestAsync(RequestT request, Endpoint<RequestT, ResponseT, ErrorT> endpoint, @Nullable TransportOptions options) {
         final OmniaEndpoint<RequestT, ResponseT, ErrorT> customEndpoint = new OmniaEndpoint<>(endpoint, sdk);
         QueryMapper mapper = new QueryMapper();
-        if (request instanceof SearchRequest) {
-            Query combinedQuery = ((SearchRequest) request).query();
-            List<String> Indexes = customEndpoint.getIndex(endpoint.requestUrl(request));
-            for (var x : Indexes) {
-                combinedQuery = sdk.addIndexFilter(combinedQuery, x);
-            }
-            try {
-                mapper.updatePrivateFields(request, combinedQuery);
-                return delegate.performRequestAsync(request, customEndpoint, options);
-            } catch (IllegalAccessException |NoSuchFieldException e) {
-                throw new RuntimeException(e);
-            }
+        Query combinedQuery = (Query) mapper.executeQuery(request);
+        if(combinedQuery == null){
+            return delegate.performRequestAsync(request, customEndpoint, options);
         }
-        return delegate.performRequestAsync(request, customEndpoint, options);
+        List<String> Indexes = customEndpoint.getIndex(endpoint.requestUrl(request));
+        for (var x : Indexes) {
+            combinedQuery = sdk.addIndexFilter(combinedQuery, x);
+        }
+        try {
+            mapper.updatePrivateFields(request, combinedQuery);
+            return delegate.performRequestAsync(request, customEndpoint, options);
+        } catch (IllegalAccessException |NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
